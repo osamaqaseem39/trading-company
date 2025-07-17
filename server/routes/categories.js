bae3255d0dd4f809d20c1518cfe4b231f9640409
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-17 14:33:20 +0500

    Initial commit

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -7,13 +7,14 @@ const categoryController = require('../controllers/categoryController');
 router.post('/', upload.single('image'), categoryController.createCategory);
 // GET /api/categories
 router.get('/', categoryController.getCategories);
+
+// GET /api/categories/nested
+router.get('/nested', categoryController.getNestedCategories);
 // GET /api/categories/:id
 router.get('/:id', categoryController.getCategory);
 // PUT /api/categories/:id
 router.put('/:id', upload.single('image'), categoryController.updateCategory);
 // DELETE /api/categories/:id
 router.delete('/:id', categoryController.deleteCategory);
-// GET /api/categories/nested
-router.get('/nested', categoryController.getNestedCategories);
 
 module.exports = router; 
