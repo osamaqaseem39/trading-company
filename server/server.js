5a6b986f44c40fba823f1c96774ccbd951c7618f
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-18 14:08:34 +0500

    Initial commit

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ app.use(cors({
     'https://trading-company.vercel.app',
     'https://server.wingzimpex.com',
     'https://server.wingzimpex.com',
-    'https://wingzimpex.com'
+    'https://wingzimpex.com',
+    'https://trading-company-website.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -59,8 +60,8 @@ app.use((req, res, next) => {
     'https://trading-company.vercel.app',
     'https://server.wingzimpex.com',
     'https://server.wingzimpex.com',
-    'https://wingzimpex.com'
-    
+    'https://wingzimpex.com',
+    'https://trading-company-website.vercel.app'
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
