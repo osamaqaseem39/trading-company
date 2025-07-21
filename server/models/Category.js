e2fb569f053d9f8ecce392f448a962ff56fe547d
osamaqaseem39 <osama.qa.39@gmail.com>
2025-07-21 10:39:09 +0500

    Initial commit

--- server/models/Category.js
+++ server/models/Category.js
@@ -4,6 +4,7 @@ const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   image: { type: String }, // file path
   description: { type: String },
+  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', default: null },
   createdAt: { type: Date, default: Date.now }
 });
 
